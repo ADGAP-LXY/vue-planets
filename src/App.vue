ce1c424079a4a2d835c6a6fd7aa820895651cf13
lvxunyi <lxy@imac-x.local>
2021-12-07 02:39:14 +0800

    will complete

--- src/App.vue
+++ src/App.vue
@@ -7,98 +7,187 @@
       every planet in the solar system and the Sun, <br />
       and beyond.
     </p>
-  </div>
-
-  <div class="planets" :style="{ width: planetsWidth + 'px'}">
-    <div v-for="item in planets" :key="item.status" :style="{ width: planetWidth + 'px' ,height: planetWidth + 'px'}" class="planet" >
-      <img :src="item.image" />
-    </div>
+    <p>
+      winWidth: {{ winWidth }}, left: {{ left }}, N: {{ N }}, pointer:
+      {{ pointer }}
+    </p>
   </div>
 
   <div
-    class="
-      swiper-container
-      lg:mt-0
-      lg:mb-0
-      mb-14
-      mt-3
-      swiper-container-initialized
-      swiper-container-horizontal
-      swiper-container-pointer-events
-      opacity-100
-    "
+    class="planets"
+    :style="{
+      left: left + 'px',
+      width: planetsWidth + 'px',
+      transition: offset === 0 ? `left ${moveSpeed}ms ease-in-out` : '',
+    }"
   >
-    <div>
-      <a href=""></a>
+    <div
+      v-for="(planet, index) in viewPlanets"
+      :key="pointer + index"
+      :style="{
+        width: planetWidth + 'px',
+        height: planetWidth + 'px',
+        opacity: 1 - (Math.abs(index - (N + 1) / 2) * 1) / (N - 2),
+        transform: `scale(${
+          1 - (Math.abs(index - (N + 1) / 2) * 1) / (N + 2)
+        })`,
+        transition: `opacity ${moveSpeed}ms ease-in-out, transform ${moveSpeed}ms ease-in-out`,
+      }"
+      class="planet"
+    >
+      <img :src="planet.image" />
+
+      <div class="planet-text">
+        <p class="p1">
+          {{ planet.name }}
+        </p>
+        <p class="p2">Current missions: {{ planet.missionStatus.current }}</p>
+        <p class="p3">Past missions: {{ planet.missionStatus.past }}</p>
+        <span class="explore">
+          Explore
+          <svg
+            width="8"
+            height="14"
+            viewBox="0 0 8 14"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            focusable="false"
+            class="IconCaret ml-1 text-base"
+          >
+            <path
+              d="M7.864 7.004L1.5 13.368.086 11.954l4.948-4.95-4.948-4.95L1.5.64l6.364 6.364z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </span>
+      </div>
     </div>
+  </div>
+
+  <div class="button left-button" @click="moveRight">
+    <Button />
+  </div>
+
+  <div class="button right-button" @click="moveLeft">
+    <Button style="transform: rotate(180deg)" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Button from "./components/Button.vue";
+
+const planetWidth = 400;
+const moveSpeed = 500;
 
 export default {
   data() {
     return {
-      name: "App",
-      planets: [],
-      planetWidth: 500,
+      allPlanets: [],
+      planetWidth,
+      moveSpeed,
+      pointer: 0,
+      offset: 0,
+      clickable: true,
+      winWidth: window.innerWidth,
     };
   },
   mounted() {
+    window.addEventListener("resize", () => {
+      this.winWidth = window.innerWidth;
+    });
     fetch("/data.json")
       .then((response) => response.json())
       .then((json) => {
-        this.planets.push(...json);
+        this.allPlanets.push(...json);
       });
   },
   computed: {
     planetsWidth() {
-      return this.planets.length * this.planetWidth
-    }
+      return (this.N + 2) * this.planetWidth;
+    },
+    N() {
+      const n = Math.ceil(this.winWidth / this.planetWidth);
+      if (n % 2 == 0) {
+        return n + 1; //确保我们得到的n是奇数，便于pointer定位指针的位置
+      }
+      return n;
+    },
+    viewPlanets() {
+      if (this.allPlanets.length === 0) {
+        return [];
+      } //当Json还没解析完成时，返回一个空数组 数据加载是async的，而computed会立刻计算属性
+      //所以我们需要等星球数据加载完成，再去计算。
+
+      let pointer = this.pointer % this.allPlanets.length;
+      if (pointer < 0) {
+        pointer += this.allPlanets.length;
+      }
+      const arr = [this.allPlanets[pointer]]; //加载完成首页最中间的图片
+      // right
+      const wingCount = (this.N - 1) / 2 + 1; //冗余：当winwidth手动改变时产生的边界问题
+      let nextPointer = pointer + 1;
+      let previousPointer = pointer - 1;
+      for (let i = 0; i < wingCount; i++) {
+        if (nextPointer >= this.allPlanets.length) {
+          nextPointer = 0;
+        }
+        arr.push(this.allPlanets[nextPointer]);
+        nextPointer++;
+      }
+      for (let i = 0; i < wingCount; i++) {
+        if (previousPointer < 0) {
+          previousPointer = this.allPlanets.length - 1;
+        }
+        arr.unshift(this.allPlanets[previousPointer]);
+        previousPointer--;
+      }
+
+      return arr;
+    },
+    left() {
+      return this.offset + (this.winWidth - this.planetsWidth) / 2;
+    },
   },
-  
+
+  methods: {
+    moveLeft() {
+      if (!this.clickable) {
+        return;
+      }
+
+      this.clickable = false;
+      this.pointer++;
+      this.offset = this.planetWidth;
+
+      setTimeout(() => {
+        this.offset = 0;
+      }, 0);
+      setTimeout(() => {
+        this.clickable = true;
+      }, this.moveSpeed);
+    },
+    moveRight() {
+      if (!this.clickable) {
+        return;
+      }
+
+      this.clickable = false;
+      this.pointer--;
+      this.offset = -this.planetWidth;
+
+      setTimeout(() => {
+        this.offset = 0;
+      }, 0);
+      setTimeout(() => {
+        this.clickable = true;
+      }, this.moveSpeed);
+    },
+  },
+  components: {
+    Button,
+  },
 };
 </script>
 
 <style>
-body {
-  background-image: url("/images/bg.jpg");
-}
-p {
-  display: block;
-  margin-block-start: 1em;
-  margin-block-end: 1em;
-  margin-inline-start: 0px;
-  margin-inline-end: 0px;
-}
-.text-white {
-  --text-opacity: 1;
-  color: #fff;
-  color: rgba(255, 255, 255, var(--text-opacity));
-}
-.col-start-2 {
-  grid-column-start: 2;
-}
-.px-4 {
-  padding-left: 1rem;
-  padding-right: 1rem;
-}
-.order-1 {
-  order: 1;
-}
-
-div {
-  display: block;
-}
-.text-white {
-  --text-opacity: 1;
-  color: #fff;
-  color: rgba(255, 255, 255, var(--text-opacity));
-}
-.bg-black {
-  --bg-opacity: 1;
-  background-color: #000;
-  background-color: rgba(0, 0, 0, var(--bg-opacity));
-}
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <button>
+    <svg viewBox="0 0 50 50" class="IconPrev">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M29 13.686l1.414 1.415-9.9 9.899 9.9 9.9L29 36.313 17.686 25 29 13.686z"
+        fill="currentColor"
+        aria-hidden="true"
+        focusable="false"
+      ></path>
+    </svg>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+};
+</script>
+
+<style scoped>
+button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  background-color: #e73b54;
+  border: none;
+}
+button:hover {
+  background: rgb(29, 177, 9);
+}
+</style>
